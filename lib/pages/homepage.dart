5aa4b978a8121d70059676ddb0cb64e63003ae8d
Khalil <khalilhenane@hotmail.com>
2025-01-29 11:14:49 +0100

    Added welcome container,  and action item

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -18,7 +18,7 @@ class _HomepageState extends State<HomePage> {
        decoration: BoxDecoration(
         gradient: LinearGradient(begin: Alignment.topLeft,  end: Alignment.bottomRight, colors: [
 
-           Color(0xFF6448FE),
+            Color(0xFF6448FE),
               Color(0xFF5FC6FF),
         ])
        ),
@@ -28,6 +28,21 @@ class _HomepageState extends State<HomePage> {
       child: Column(
         children: [
           customAppBar(),
+          // SizedBox(height: 24,), 
+          Expanded(
+          child: SingleChildScrollView(
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                welcomeContainer(),
+                SizedBox(height: 24,),
+                actionsContainer(context),
+
+              ],
+              
+            ),
+          ),)
         ],
       )
       
@@ -46,6 +61,8 @@ class _HomepageState extends State<HomePage> {
         children: [
           Text("Flashcard App", style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)),
 
+    // TODO Add here a iconbutton to change the   theme style.
+      // IconButton(onPressed: () {}, icon: Icon(Icons.circle_)),
           CircleAvatar(
                 backgroundColor: Colors.white.withAlpha(77),
 
@@ -56,4 +73,88 @@ class _HomepageState extends State<HomePage> {
       ),
     );
   }
+
+  Widget welcomeContainer() {
+
+     return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white.withAlpha(38),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //TODO Later add when user is logged in output: "Welcome back" else just "Welcome"
+          Text("Welcom back", style: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),)
+,
+            Text("Ready to continue learning?", style: GoogleFonts.poppins(fontSize: 16, color: Colors.white.withAlpha(204)),)
+
+
+        ],
+      ),
+     );
+  }
+
+
+   Widget actionsContainer(BuildContext context) {
+
+    return Column(
+
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+       //* Maby change this into "Quik actions" not sure yet 
+        Text("Actions", style: GoogleFonts.poppins(fontSize: 20, color: Colors.white,  fontWeight: FontWeight.bold))
+      ,
+        SizedBox(height: 30,)
+      , Row(
+
+        children: [
+          Expanded(child:  actionButton(context, "Public flashcards", Icons.public , () {} ,    ),), 
+
+        ],
+      )
+      ],
+    );
+  }
+  Widget actionButton(BuildContext context, String title,  IconData icon, VoidCallback ontap,) {
+
+
+    return GestureDetector(
+
+      onTap: ontap, 
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius:  BorderRadius.circular(16),
+          boxShadow: [
+
+            BoxShadow(color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset(0, 4)
+            )
+
+
+          ]
+        ),
+child: Column(
+  mainAxisSize:  MainAxisSize.min,
+
+  children: [
+    Icon(icon, size: 32,
+    
+                  color: Color(0xFF6448FE),
+
+    ),
+    SizedBox(height: 12,),
+    Text(title, style:  GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600,                 color: Color(0xFF6448FE),
+ ),)
+  ],
+),
+      ),
+    );
+  }
+ 
+
 }
