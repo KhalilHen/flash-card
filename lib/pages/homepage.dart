6dc8f1d4d50b4a3130c9f980bb8d7d6efb09ceb5
Khalil <khalilhenane@hotmail.com>
2025-01-30 15:15:53 +0100

    Added different gradient colors for different themes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,84 +15,73 @@ class _HomepageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-      body:  Container(
-       decoration: BoxDecoration(
-        gradient: LinearGradient(begin: Alignment.topLeft,  end: Alignment.bottomRight, colors: [
-
-            Color(0xFF6448FE),
-              Color(0xFF5FC6FF),
-        ])
-       ),
-       //Safeare to prevent different mobile device having different screen
-    child: SafeArea(
-      
-      child: Column(
-        children: [
-          customAppBar(),
-          // SizedBox(height: 24,), 
-          Expanded(
-          child: SingleChildScrollView(
-
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
+          Color(0xFF6448FE),
+          Color(0xFF5FC6FF),
+        ])),
+        //Safeare to prevent different mobile device having different screen
+        child: SafeArea(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                welcomeContainer(),
-                SizedBox(height: 24,),
-                actionsContainer(context),
-
-              ],
-              
-            ),
-          ),)
-        ],
-      )
-      
+          children: [
+            customAppBar(),
+            // SizedBox(height: 24,),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    welcomeContainer(),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    actionsContainer(context),
+                  ],
+                ),
+              ),
+            )
+          ],
+        )),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Custom app bar
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DisplayFlashcardSets()),
+            // MaterialPageRoute(builder: (context) => CreateFlashcardSet()),
+          );
+        },
+        child: Icon(Icons.add),
       ),
-    floatingActionButton: FloatingActionButton(onPressed: () {
-
-
-   
-  //Custom app bar 
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => DisplayFlashcardSets()),
-        // MaterialPageRoute(builder: (context) => CreateFlashcardSet()),
-
-    
-  );
-    },
-    child: Icon(Icons.add),
-    ),
     );
   }
+
   Widget customAppBar() {
     return Container(
-
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text("Flashcard App", style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)),
 
-    // TODO Add here a iconbutton to change the   theme style.
-      // IconButton(onPressed: () {}, icon: Icon(Icons.circle_)),
+          // TODO Add here a iconbutton to change the   theme style.
+          // IconButton(onPressed: () {}, icon: Icon(Icons.circle_)),
           CircleAvatar(
-                backgroundColor: Colors.white.withAlpha(77),
-
-              child: IconButton(onPressed: () {}, icon:  Icon(Icons.person), ),
+            backgroundColor: Colors.white.withAlpha(77),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.person),
+            ),
           )
-      
         ],
       ),
     );
   }
 
   Widget welcomeContainer() {
-
-     return Container(
-      
+    return Container(
       padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
         color: Colors.white.withAlpha(38),
@@ -102,82 +91,85 @@ class _HomepageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           //TODO Later add when user is logged in output: "Welcome back" else just "Welcome"
-          Text("Welcom back", style: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),)
-,
-            Text("Ready to continue learning?", style: GoogleFonts.poppins(fontSize: 16, color: Colors.white.withAlpha(204)),)
-
-
+          Text(
+            "Welcom back",
+            style: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          Text(
+            "Ready to continue learning?",
+            style: GoogleFonts.poppins(fontSize: 16, color: Colors.white.withAlpha(204)),
+          )
         ],
       ),
-     );
+    );
   }
 
-
-   Widget actionsContainer(BuildContext context) {
-
+  Widget actionsContainer(BuildContext context) {
     return Column(
-
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-       //* Maby change this into "Quik actions" not sure yet 
-        Text("Actions", style: GoogleFonts.poppins(fontSize: 20, color: Colors.white,  fontWeight: FontWeight.bold))
-      ,
-        SizedBox(height: 30,)
-      , Row(
-
-        children: [
-          Expanded(child:  actionButton(context, "Public flashcards", Icons.public , () {} ,    ),), 
-    SizedBox(width: 25,),
-    Expanded(child: actionButton(context, "My sets", Icons.folder,  () {
-
-      
-    }))
-        ],
-      )
+        //* Maby change this into "Quik actions" not sure yet
+        Text("Actions", style: GoogleFonts.poppins(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: actionButton(
+                context,
+                "Public flashcards",
+                Icons.public,
+                () {},
+              ),
+            ),
+            SizedBox(
+              width: 25,
+            ),
+            Expanded(child: actionButton(context, "My sets", Icons.folder, () {}))
+          ],
+        )
       ],
     );
   }
-  Widget actionButton(BuildContext context, String title,  IconData icon, VoidCallback ontap,) {
 
-
+  Widget actionButton(
+    BuildContext context,
+    String title,
+    IconData icon,
+    VoidCallback ontap,
+  ) {
     return GestureDetector(
-
-      onTap: ontap, 
+      onTap: ontap,
       child: Container(
         padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
-          // color: Colors.white,
-          color: Colors.grey[100], //giving bit of grey tint
-          borderRadius:  BorderRadius.circular(16),
-          boxShadow: [
-
-            BoxShadow(color: Colors.black.withAlpha(25),
-            blurRadius: 10,
-            offset: Offset(0, 4)
+            // color: Colors.white,
+            color: Colors.grey[100], //giving bit of grey tint
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [BoxShadow(color: Colors.black.withAlpha(25), blurRadius: 10, offset: Offset(0, 4))]),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              size: 32,
+              color: Color(0xFF6448FE),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              title,
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFF6448FE),
+              ),
             )
-
-
-          ]
+          ],
         ),
-child: Column(
-  mainAxisSize:  MainAxisSize.min,
-
-  children: [
-    Icon(icon, size: 32,
-    
-                  color: Color(0xFF6448FE),
-
-    ),
-    SizedBox(height: 12,),
-    Text(title, style:  GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600,                 color: Color(0xFF6448FE),
- ),)
-  ],
-),
       ),
     );
- 
   }
-  
- 
-
 }
---
+++ lib/style/theme_styles.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+//!  Colors source:  https://www.b3multimedia.ie/beautiful-color-gradients-for-your-next-design-project/
+// Ocean blue
+const Color oceanBlueStart = Color(0xFF00B686);
+const Color oceanBlueEnd = Color(0xFF1BFFFF);
+
+// Sanguine theme
+const Color sanguineStart = Color(0xFFD4145A);
+const Color sanguineEnd = Color(0xFFFBB03B);
+
+// Lucious Lime theme
+const Color luciousLimeStart = Color(0xFF009245);
+const Color luciousLimeEnd = Color(0xFFFCEE21);
+
+// Green beach theme
+const Color greenBeachStart = Color(0xFF02AABD);
+const Color greenBeachEnd = Color(0xFF00CDAC);
+
+// Mountain Rock theme
+const Color mountainRockStart = Color(0xFF868F96);
+const Color mountainRockEnd = Color(0xFF596164);
+
+// No mans theme
+const Color noMansStart = Color(0xFFA9F1DF);
+const Color noMansEnd = Color(0xFFFFBBBB);
+
+// Toxic theme
+const Color toxicStart = Color(0xFFBFF098);
+const Color toxicEnd = Color(0xFF6FD6FF);
+
+// Exotic theme
+const Color exoticStart = Color(0xFFFF61D2);
+const Color exoticEnd = Color(0xFFFE9090);
+
+// Cactus theme
+const Color cactusStart = Color(0xFFC6EA8D);
+const Color cactusEnd = Color(0xFFFE90AF);
+
+// Socialive theme
+const Color socialiveStart = Color(0xFF06BEB6);
+const Color socialiveEnd = Color(0xFF48B1BF);
+
+final LinearGradient oceanBlueGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [oceanBlueStart, oceanBlueEnd],
+);
+
+final LinearGradient sanguineGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [sanguineStart, sanguineEnd],
+);
+
+final LinearGradient luciousLimeGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [luciousLimeStart, luciousLimeEnd],
+);
+
+final LinearGradient greenBeachGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [greenBeachStart, greenBeachEnd],
+);
+
+final LinearGradient mountainRockGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [mountainRockStart, mountainRockEnd],
+);
+
+final LinearGradient noMansGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [noMansStart, noMansEnd],
+);
+
+final LinearGradient toxicGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [toxicStart, toxicEnd],
+);
+
+final LinearGradient exoticGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [exoticStart, exoticEnd],
+);
+
+final LinearGradient cactusGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [cactusStart, cactusEnd],
+);
+
+final LinearGradient socialiveGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [socialiveStart, socialiveEnd],
+);
